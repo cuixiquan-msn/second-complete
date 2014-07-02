c0c046f5af163b8465bbe60553cd626fe604e264
cuixiquan <cuixiquan@gmail.com>
2014-07-01 21:18:52 -0700

    Add chapter 6 excercise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -61,10 +61,20 @@ describe User do
 		it { should_not be_valid}
 	end
 
+	describe "email should be in lower case" do
+		before do 
+			@user.email="USER@EXAMPLE.COM"
+			@user.save
+			@user.reload
+		end
+		
+		its(:email) { should eq "user@example.com" }
+	end
+
 	describe "when email format is invalid" do
 		it "should be invalid" do
 			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-			foo@bar_baz.com foo@bar+baz.com]
+			foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 			addresses.each do |invalid_address|
 			@user.email = invalid_address
 			expect(@user).not_to be_valid
